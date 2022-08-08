b6e9174f198a4c45daaf5519d3d531df084a7f44
jkhan24558 <52003763+jkhan24558@users.noreply.github.com>
2022-08-08 11:53:07 -0400

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu Live Demo", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu Live Demo!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu Live Demo"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu Live Demo!"));
     }
 }
